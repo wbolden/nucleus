18ad880e398140991cf0b939f0655f938b79729e
aertran10208 <ryantran@gmail.com>
2017-05-12 21:11:28 -0700

    update vis scaling

--- index.js
+++ index.js
@@ -531,7 +531,8 @@ function redraw(size){
       function zoomTo(v) {
         var k = diameter / v[2]; view = v;
         node.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
-        circle.attr("r", function(d) { return d.r * k; });
+        svg1.selectAll("circle")
+            .attr("r", function(d) { return d.r * k; });
         
 
           //svg1.selectAll("text").style( "font-size", function(d) {return d.r * (10/d.cp.length) *k/3 });
